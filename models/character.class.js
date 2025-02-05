b0bac0602840eb2fc6110f8680bbefbc6a6ea146
Leon Dorsch <leon.dorsch@gmx.de>
2025-02-05 21:31:00 +0100

    added motion to chickend and character

--- models/character.class.js
+++ models/character.class.js
@@ -1,9 +1,33 @@
 class Character extends MoveableObject {
-    constructor(){
-        super().loadImage('img/2_character_pepe/2_walk/W-21.png');
+    height = 290;
+    y = 155;
+    width = 120;
+    IMAGES_WALKING = [
+        'img/2_character_pepe/2_walk/W-21.png',
+        'img/2_character_pepe/2_walk/W-22.png',
+        'img/2_character_pepe/2_walk/W-23.png',
+        'img/2_character_pepe/2_walk/W-24.png',
+        'img/2_character_pepe/2_walk/W-25.png',
+        'img/2_character_pepe/2_walk/W-26.png'
+    ]
+
+    constructor() {
+        super();
+        this.loadImage('img/2_character_pepe/2_walk/W-21.png');
+        this.loadImages(this.IMAGES_WALKING);
+        this.animate();
     }
 
-    jump(){
+    animate() {
+        setInterval(() => {
+            let i = this.currentImage % this.IMAGES_WALKING.length;
+            let path = this.IMAGES_WALKING[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }, 100);
+    };
+
+    jump() {
 
     }
 }
--- models/world.class.js
+++ models/world.class.js
@@ -9,7 +9,11 @@ class World {
         new Cloud()
     ]
     backgroundObjects = [
-        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0, 100)
+        new BackgroundObject('img/5_background/layers/air.png', 0),
+        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0),
+        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0),
+        
     ]
     ctx;
     canvas;
@@ -23,10 +27,10 @@ class World {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-        this.addToMap(this.character);
-        this.addObjectsToMap(this.clouds);
-        this.addObjectsToMap(this.enemies);
         this.addObjectsToMap(this.backgroundObjects);
+        this.addObjectsToMap(this.clouds);
+        this.addToMap(this.character);
+        this.addObjectsToMap(this.enemies);
 
         let self = this;
         requestAnimationFrame(function () {
